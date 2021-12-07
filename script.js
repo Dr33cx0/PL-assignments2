218572728d222f30479dfa6ad28171c4e346986e
BrunoSilva077 <95039120+BrunoSilva077@users.noreply.github.com>
2021-12-07 13:44:25 +0000

    Update script.js
    Fiz alterações no sistema de validação dos binários.

--- script.js
+++ script.js
@@ -15,9 +15,17 @@ function calcBin(){
   var input= Number(document.getElementById("num").value);
   var input1= (document.getElementById("num").value);
   
-    
+    if(document.getElementById("nums1").value=='16' && document.getElementById("selected").value=='10'){
+      for(var i = input1.length-1; i>= 0; i--){
+        if(input1[i] != 'a' && input1[i] != 'b' && input1[i] != 'c' && input1[i] != 'd' && input1[i] != 'e' && input1[i] != 'f' && input1[i] != 1 && input1[i] != 2 && input1[i] != 3 && input1[i] != 4 && input1[i] != 5 && input1[i] != 6 && input1[i] != 7 && input1[i] != 8 && input1[i] != 9){
+            return document.getElementById("result").innerHTML = "Repare que não está a inserir um número hexadecimal correto";
+        }
+    }
+      var input2=parseInt(input1,16);
+    document.getElementById("result").innerHTML = "Decimal: "+input2.toString(10);
+  }
     //de base octal para qualquer outra base
-     if(document.getElementById("nums1").value=='8' && document.getElementById("selected").value=='2'){
+    else if(document.getElementById("nums1").value=='8' && document.getElementById("selected").value=='2'){
       input=parseInt(input,8);
       document.getElementById("result").innerHTML = "Binário: "+input.toString(2);
      }
@@ -50,7 +58,7 @@ function calcBin(){
     else if(document.getElementById("nums1").value=='16' && document.getElementById("selected").value=='2'){
       for(var i = input1.length-1; i>= 0; i--){
         if(input1[i] != 'a' && input1[i] != 'b' && input1[i] != 'c' && input1[i] != 'd' && input1[i] != 'e' && input1[i] != 'f' && input1[i] != 1 && input1[i] != 2 && input1[i] != 3 && input1[i] != 4 && input1[i] != 5 && input1[i] != 6 && input1[i] != 7 && input1[i] != 8 && input1[i] != 9){
-            return document.getElementById("result").innerHTML = "Repare que não está a inserir um número hexadecimal correto";
+            return document.getElementById("result").innerHTML = "Repare que não está a inserir um número hexadecimal";
         }
       var input2=parseInt(input1,16);
     document.getElementById("result").innerHTML = "Decimal: "+input2.toString(2);
@@ -59,62 +67,56 @@ function calcBin(){
     else if(document.getElementById("nums1").value=='16' && document.getElementById("selected").value=='8'){
       for(var i = input1.length-1; i>= 0; i--){
         if(input1[i] != 'a' && input1[i] != 'b' && input1[i] != 'c' && input1[i] != 'd' && input1[i] != 'e' && input1[i] != 'f' && input1[i] != 1 && input1[i] != 2 && input1[i] != 3 && input1[i] != 4 && input1[i] != 5 && input1[i] != 6 && input1[i] != 7 && input1[i] != 8 && input1[i] != 9){
-            return document.getElementById("result").innerHTML = "Repare que não está a inserir um número hexadecimal correto";
+            return document.getElementById("result").innerHTML = "Repare que não está a inserir um número hexadecimal";
         }
       var input2=parseInt(input1,16);
     document.getElementById("result").innerHTML = "Decimal: "+input2.toString(8);
     }
   }
-    else if(document.getElementById("nums1").value=='16' && document.getElementById("selected").value=='10'){
-    for(var i = input1.length-1; i>= 0; i--){
-      if(input1[i] != 'a' && input1[i] != 'b' && input1[i] != 'c' && input1[i] != 'd' && input1[i] != 'e' && input1[i] != 'f' && input1[i] != 1 && input1[i] != 2 && input1[i] != 3 && input1[i] != 4 && input1[i] != 5 && input1[i] != 6 && input1[i] != 7 && input1[i] != 8 && input1[i] != 9){
-          return document.getElementById("result").value = "Repare que não está a inserir um número hexadecimal correto";
-      }
-  }
-    var input2=parseInt(input1,16);
-  document.getElementById("result").innerHTML = "Decimal: "+input2.toString(10);
-}
     else if(document.getElementById("nums1").value=='16' && document.getElementById("selected").value=='16'){
       for(var i = input1.length-1; i>= 0; i--){
         if(input1[i] != 'a' && input1[i] != 'b' && input1[i] != 'c' && input1[i] != 'd' && input1[i] != 'e' && input1[i] != 'f' && input1[i] != 1 && input1[i] != 2 && input1[i] != 3 && input1[i] != 4 && input1[i] != 5 && input1[i] != 6 && input1[i] != 7 && input1[i] != 8 && input1[i] != 9){
-            return document.getElementById("result").innerHTML = "Repare que não está a inserir um número hexadecimal correto";
+            return document.getElementById("result").innerHTML = "Repare que não está a inserir um número hexadecimal";
         }
       document.getElementById("result").innerHTML =("Hexadecimal: "  + input1);
     }
   }
     //de base binaria para qualquer outra base
     else if(document.getElementById("nums1").value=='2' && document.getElementById("selected").value=='8'){
-      const test = ~0x01
-      for(var i=input1.length-1; i>=0; --i){
-        if(test & input1[i]){ return document.getElementById("result").innerHTML =("Repare que não está a inserir um número binário"); }
-      }
+      for(var i = input1.length-1; i>= 0; i--){
+        if(input1[i] != 1 && input1[i] != 0) {
+            return document.getElementById("result").innerHTML = "Repare que não está a inserir um número binário, porfavor insira um número que contenha apenas '0' e '1'";
+        }
       input1=parseInt(input1,2);
       return document.getElementById("result").innerHTML = "Octal: "+input1.toString(8);
-      
+      }
     }
     else if(document.getElementById("nums1").value=='2' && document.getElementById("selected").value=='10'){
-      const test = ~0x01
-      for(var i=input1.length-1; i>=0; --i){
-        if(test & input1[i]){ return document.getElementById("result").innerHTML =("Repare que não está a inserir um número binário"); }
-      }
+      for(var i = input1.length-1; i>= 0; i--){
+        if(input1[i] != 1 && input1[i] != 0) {
+            return document.getElementById("result").innerHTML = "Repare que não está a inserir um número binário, porfavor insira um número que contenha apenas '0' e '1'";
+        }
       input1=parseInt(input1,2);
       return document.getElementById("result").innerHTML = "Decimal: "+input1;
+      }
     }
 
     else if(document.getElementById("nums1").value=='2' && document.getElementById("selected").value=='16'){
-      const test = ~0x01
-      for(var i=input1.length-1; i>=0; --i){
-        if(test & input1[i]){ return document.getElementById("result").innerHTML =("Repare que não está a inserir um número binário"); }
-      }
+      for(var i = input1.length-1; i>= 0; i--){
+        if(input1[i] != 1 && input1[i] != 0) {
+            return document.getElementById("result").innerHTML = "Repare que não está a inserir um número binário, porfavor insira um número que contenha apenas '0' e '1'";
+        }
       input1=parseInt(input1,2);
-      return document.getElementById("result").innerHTML = "Hexadecimal: "+input1.toString(16);
+      return document.getElementById("result").innerHTML = "Hexadecimal: "+input1.toString(8);
+      }
     }
 
     else if(document.getElementById("nums1").value=='2' && document.getElementById("selected").value=='2'){
-      const test = ~0x01
-      for(var i=input1.length-1; i>=0; --i){
-        if(test & input1[i]){ return document.getElementById("result").innerHTML =("Repare que não está a inserir um número binário"); }
-      }
-      return document.getElementById("result").innerHTML = "Binario: "+input;
-    }
+      for(var i = input1.length-1; i>= 0; i--){
+        if(input1[i] != 1 && input1[i] != 0){
+            return document.getElementById("result").innerHTML = "Repare que não está a inserir um número binário, porfavor insira um número que contenha apenas '0' e '1'";
+        }
+      document.getElementById("result").innerHTML =("Binário: "  + input1);
+    }  
+  }
   }   
